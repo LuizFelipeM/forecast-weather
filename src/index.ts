2d85fb1820d6f2f51cc7a12a815d735fb5050397
LuizFelipeM <muniz.mourafelipe@gmail.com>
2021-04-09 18:01:27 -0300

    Modified index.html

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-const app = express()
-const port = process.env.PORT ?? 5000
-
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile('index.html', {root: __dirname + '/public/'}))
-app.listen(port, () => console.log(`Running application in port ${port}`))
